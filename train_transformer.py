da9d79184b577c648ff97d254fa94332865316dc
eyalbetzalel <eyalbatzul@gmail.com>
2020-07-24 18:16:23 +0300

    ImageNet64 changes

--- train_transformer.py
+++ train_transformer.py
@@ -113,7 +113,14 @@ def main():
             transforms.Resize(config.model.image_size),
             transforms.ToTensor()
         ])
-
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    print()
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_cached(0) / 1024 ** 3, 1), 'GB')
     path = r"/home/dsi/eyalbetzalel/glow/GLOW/GLOW/imagenet64/"
     print("Load ImageNet (long)")
     dataset = datasets.ImageFolder(path, transform=transform)
